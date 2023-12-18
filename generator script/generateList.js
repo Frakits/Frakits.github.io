4d2e03ee83128a6e6e749f8f55cfea59782864d0
Frakits <xgamerxthegamer@gmail.com>
2023-12-18 21:36:27 +0200

    new games + local packages

--- generator script/generateList.js
+++ generator script/generateList.js
@@ -8,43 +8,46 @@ let timer = 0;
 let listOptions = JSON.parse(fs.readFileSync("./generator script/list_options.json"));
 let progressCap = Object.entries(listOptions.games_for_recommendation).length * 2;
 
-//if (fs.existsSync("./final_listv2")) fs.renameSync("./final_listv2", "./final_listv2-old")
+if (fs.existsSync("./final_listv2")) {
+	fs.rmSync("./final_listv2-old")
+	fs.renameSync("./final_listv2", "./final_listv2-old")
+}
 Object.entries(listOptions.games_for_recommendation).forEach(([key, universeId]) => {
-	for (let i=0; i<2; i++) {
+	for (let i = 0; i < 2; i++) {
 		timer++;
 		setTimeout(() => {
-			get({host: `games.roblox.com`, agent: false, path: `/v1/games/recommendations/game/${universeId}?PaginationKey=startRowIndex_${i * 10}%2Cversion_&MaxRows=5000&IsTruncatedResultsEnabled=false`})
-			.then(res2 => {
-				let jsonFromRoblox = JSON.parse(res2.body).games;
-				if (jsonFromRoblox == null) {
-					progress++;
-					if (progress == progressCap) {
-						games = fixList(games)
+			get({ host: `games.roblox.com`, agent: false, path: `/v1/games/recommendations/game/${universeId}?PaginationKey=startRowIndex_${i * 10}%2Cversion_&MaxRows=5000&IsTruncatedResultsEnabled=false` })
+				.then(res2 => {
+					let jsonFromRoblox = JSON.parse(res2.body).games;
+					if (jsonFromRoblox == null) {
+						progress++;
+						if (progress == progressCap) {
+							games = fixList(games)
+							fs.writeFileSync("./final_listv2.json", JSON.stringify(games, null, 2));
+						}
+						return;
+					}
+					Object.entries(jsonFromRoblox).forEach(([key2, value]) => {
+						let properGameJson = {
+							uid: value.universeId,
+							n: value.name,
+							l: value.totalUpVotes,
+							dl: value.totalDownVotes,
+							id: value.placeId,
+							pls: value.playerCount,
+							cid: value.creatorId,
+							ct: value.creatorType,
+							d: value.gameDescription
+						}
+						games.push(properGameJson);
+						progress += 1 / Object.entries(jsonFromRoblox).length;
+						console.log(`${progress.toFixed(0)}/${progressCap}`);
+					});
+					if (progress.toFixed(0) == progressCap) {
+						games = fixList(games);
 						fs.writeFileSync("./final_listv2.json", JSON.stringify(games, null, 2));
 					}
-					return;
-				}
-				Object.entries(jsonFromRoblox).forEach(([key2, value])=>{
-					let properGameJson = {
-						uid: value.universeId,
-						n: value.name,
-						l: value.totalUpVotes,
-						dl: value.totalDownVotes,
-						id: value.placeId,
-						pls: value.playerCount,
-						cid: value.creatorId,
-						ct: value.creatorType,
-						d: value.gameDescription
-					}
-					games.push(properGameJson);
-					progress += 1 / Object.entries(jsonFromRoblox).length;
-					console.log(`${progress.toFixed(0)}/${progressCap}`);
-				});
-				if (progress.toFixed(0) == progressCap) {
-					games = fixList(games);
-					fs.writeFileSync("./final_listv2.json", JSON.stringify(games, null, 2));
-				}
-			})
+				})
 		}, 600 * timer);
 	}
 })
